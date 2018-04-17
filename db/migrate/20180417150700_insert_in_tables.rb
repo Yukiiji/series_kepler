ccbe4f4adcfc69ca4216166ecea3d32457b51e67
Raph'A <raph.yukiji@gmail.com>
2018-04-17 16:57:07 +0200

    heroku accueil

--- db/migrate/20180417150700_insert_in_tables.rb
+++
@@ -1,8 +0,0 @@
-class InsertInTables < ActiveRecord::Migration[5.1]
-  def change
-    restaurant = Restaurant.new(attributes)
-    if restaurant.valid?
-      restaurant.save!
-    else
-  end
-end
