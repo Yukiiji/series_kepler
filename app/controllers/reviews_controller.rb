e42d635f71748b7e6b008dbbafbd737841746480
Raph'A <raph.yukiji@gmail.com>
2018-06-02 11:44:52 +0200

    Push form non complet

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,28 @@
 class ReviewsController < ApplicationController
 
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @episode = Episode.find(params[:episode_id])
+    @review = Review.new(allowed_params)
+    @review.episode = @episode
+    @review.user = current_user
+
+    if @review.valid?
+      @review.save!
+      redirect_to show_episode_path
+    else
+
+    end
+  end
+
+
+  private
+
+  def allowed_params
+    params.require(:review).permit(:rating, :comment)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   resources :views
   resources :bookmarks
-  resources :episodes
-  resources :seasons
-  resources :series
-  resources :reviews
+  resources :episodes do
+    resources :reviews
+  end
+  resources :seasons do
+    resources :episodes
+  end
+  resources :series do
+    resources :seasons
+  end
+
   resources :users
   root 'series#index'
   get 'welcome/hello'
