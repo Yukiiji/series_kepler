59e75cdd1a4202af0e18cc719f7c09f487961d8d
Raph'A <raph.yukiji@gmail.com>
2018-06-04 23:30:42 +0200

    POINT FINAL BRAVO A TOUS

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -4,28 +4,27 @@ class ViewsController < ApplicationController
 
   def create
 
-    if allowed_params == true
-      @views = View.new
-      @views.user = current_user
-      @views.episode = params[:episode_id]
+    # if allowed_params != ''
+    #   @views = View.new
+    #   @views.user = current_user
+    #   @views.episode = @episode
+    #
+    #   if @views.valid?
+    #     @views.save!
+    #     redirect_to episode_path(@episode)
+    #   end
+    # else
+    #   if View.find(params[:episode_id])?
+    #     i = View.find(params[:episode_id])
+    #     i.destroy
+    #     redirect_to episode_path(@episode)
+    #   else
+    #     redirect_to episode_path(@episode)
+    #   end
+    # end
 
-      if @views.valid?
-        @views.save!
-        redirect_to episode_path(@episode)
-      end
-    else
-      if View.find(params[:episode_id]) != nil
-        destroy
-      else
-        redirect_to episode_path(@episode)
-      end
-    end
-  end
+    #Cette section ne marche pas, l'idée etait de vérifier si la checkbox était cochée, et si c'est le cs on entre l'episode vu ans la base, sinon on cherche si il existe deja dedans et si c'est le cas on le supprime (il devient non vu par l'utilisateur)
 
-  def destroy
-    i = View.find(params[:episode_id])
-    i.destroy
-    redirect_to episode_path(@episode)
   end
 
   private
